f8396da05cee00c01fe345179a2f76a64a653148
guribeiroh <agenciatdx@gmail.com>
2025-03-16 10:38:33 -0300

    Corrigir erro de tipagem no campo publication_year

--- app/admin/livros/[id]/page.tsx
+++ app/admin/livros/[id]/page.tsx
@@ -382,7 +382,7 @@ export default function EditarLivroPage({ params }: { params: { id: string } }) 
                 name="publication_year"
                 min="1800"
                 max={new Date().getFullYear()}
-                value={formData.publication_year}
+                value={formData.publication_year || ''}
                 onChange={handleChange}
                 className="block w-full border border-gray-300 rounded-md shadow-sm focus:ring-primary-500 focus:border-primary-500 py-2 px-3"
               />
